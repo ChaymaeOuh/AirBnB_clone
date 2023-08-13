df0a9f83c2548b7ebfb6b13998c9ed6bbdd03b53
ChaimaaFr <chaimaefarkouchi@gmail.com>
2023-08-13 06:22:25 -0700

    update

--- models/city.py
+++ models/city.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""City Module"""
+"""Defines the City class."""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
+    """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
     """
-    The public class attributes should be empty
-    """
+
     state_id = ""
     name = ""
